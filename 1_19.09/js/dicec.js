a233d3bebc649d20359a5470ae893c585a954c0c
שבתי גולדברג <shgoldbe@g.jct.ac.il>
2019-10-03 12:14:24 +0300

    add folder

---
+++ 1_19.09/js/dicec.js
@@ -0,0 +1,13 @@
+var promis = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve([Math.ceil(Math.random() * 6), Math.ceil(Math.random() * 6)]);
+    }, 2000);
+});
+
+promis.then((data) => {
+    if (data[0] == data[1]) {
+        console.log('double')
+    } else {
+        console.log('The first dice return - ' + data[0] + '\nThe second dice return -  ' + data[1])
+    }
+})
