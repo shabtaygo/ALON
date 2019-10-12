90c95207642835805e6a7cdbf41b7ee1b4602345
שבתי גולדברג <shgoldbe@g.jct.ac.il>
2019-10-13 00:23:28 +0300

    updata

--- nodjs_1/js/dice_3.js
+++
@@ -1,7 +0,0 @@
-var dice1 = Math.ceil(Math.random() * 6);
-var dice2 = Math.ceil(Math.random() * 6);
-if (dice1==dice2) {
-    console.log(`lucky day you get double ${dice2}`);
-} else {
-    console.log(`The result of thh first dice is:  ${dice1}\nThe result of thh second dice is:  ${dice2}`);
-}
