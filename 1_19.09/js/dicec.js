00567169d7370f9273f48031a20be6a468490ac8
שבתי גולדברג <shgoldbe@g.jct.ac.il>
2019-10-27 10:19:09 +0200

    a litel change

--- 1_19.09/js/dicec.js
+++ 1_19.09/js/dicec.js
@@ -8,6 +8,7 @@ promis.then((data) => {
     if (data[0] == data[1]) {
         console.log('double')
     } else {
-        console.log('The first dice return - ' + data[0] + '\nThe second dice return -  ' + data[1])
+       // console.log('The first dice return - ' + data[0] + '\nThe second dice return -  ' + data[1])
+       console.log(`The first dice return - ${data[0]} \nThe second dice return -   ${data[1]}`)
     }
 })
