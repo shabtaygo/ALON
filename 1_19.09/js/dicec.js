9bf68a186642d95b245726e1ed60a958d9f31bdb
shabtaygo <34300850+shabtaygo@users.noreply.github.com>
2019-10-27 10:26:27 +0200

    Update dicec.js
    change

--- 1_19.09/js/dicec.js
+++ 1_19.09/js/dicec.js
@@ -8,7 +8,6 @@ promis.then((data) => {
     if (data[0] == data[1]) {
         console.log('double')
     } else {
-       // console.log('The first dice return - ' + data[0] + '\nThe second dice return -  ' + data[1])
        console.log(`The first dice return - ${data[0]} \nThe second dice return -   ${data[1]}`)
     }
 })
